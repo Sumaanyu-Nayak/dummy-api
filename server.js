b4aa04e38328e64556e74da5080350522b60e9c2
Sumaanyu-Nayak <nayaksumaanyu@gmail.com>
2024-01-24 23:14:29 +0530

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+
+app.use(cors(
+    {
+        origin:"*",
+    }
+));
 
 const fcMenu = require('./routes/fcMenu');
 
